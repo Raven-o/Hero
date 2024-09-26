b102ab2bd54126a39963de8549a62189c1f806fc
juliavila <julcar.as@gmail.com>
2024-09-26 18:47:36 -0500

    Cambios en salto y vidas

--- Mario/src/main/java/com/mycompany/mario/Heroe.java
+++ Mario/src/main/java/com/mycompany/mario/Heroe.java
@@ -1,13 +1,134 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.mario;
+package com.mycompany.mario; 
 
-/**
- *
- * @author Usuario
- */
-public class Heroe {
+  
+class Heroe { 
+
+    private int vidas; 
+
+    private int x;  
+
+    private String nombre; 
+
+    private boolean saltando; 
+
+     
+
+    public Heroe(String nombre){ 
+
+        this.vidas=3; 
+
+        this.x=0; 
+
+        this.nombre=nombre; 
+
+        this.saltando=false; 
+
+    } 
+
+  
+
     
-}
+
+  
+
+    public int getVidas() { 
+
+        return vidas; 
+
+    } 
+
+  
+
+    public int getPosicion() { 
+
+        return x; 
+
+    } 
+
+  
+
+    public String getNombre() { 
+
+        return nombre; 
+
+    } 
+
+  
+
+    public boolean getSaltando() { 
+
+        return saltando; 
+
+    } 
+
+  
+
+     
+
+     
+
+  
+
+    public void setVidas(int vidas) { 
+
+        this.vidas = vidas; 
+
+    } 
+
+     
+
+     
+
+  
+
+    /* 
+
+        Metodos 
+
+     
+
+    */ 
+
+  
+
+  
+
+     
+
+    public void avanzarUnPaso(){ 
+
+         
+
+         
+
+        this.x= this.x+1; 
+
+        System.out.println("Avanza 1, su posicion es: "+this.x); 
+
+        this.saltando = false; 
+
+    } 
+
+     
+
+    public void saltarObstaculo(){ 
+
+         
+
+        this.x= this.x+1; 
+
+         
+
+        this.saltando=true; 
+
+        System.out.println("Sus vidas son: "+this.vidas +" estado: "+this.saltando); 
+
+    } 
+
+     
+
+} 
+
+ 
+
+ 
--- Mario/src/main/java/com/mycompany/mario/Mario.java
+++ Mario/src/main/java/com/mycompany/mario/Mario.java
@@ -1,16 +1,118 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+package com.mycompany.mario; 
 
-package com.mycompany.mario;
+  
 
-/**
- *
- * @author Usuario
- */
-public class Mario {
+import java.util.Scanner; 
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+  
+
+/** 
+
+* 
+
+* @author Usuario 
+
+*/ 
+
+public class Mario { 
+
+  
+
+    public static void main(String[] args) { 
+
+         Heroe marioB = new Heroe("MarioB"); 
+
+         
+
+         Scanner lector  = new Scanner(System.in); 
+
+         char tecla =' '; 
+
+         boolean bandera=false; 
+
+         System.out.println("Bienvenido a MarioB"); 
+
+         System.out.println("Su jugador es: "+marioB.getNombre()+" tiene "+marioB.getVidas()+" vidas. "); 
+
+          
+
+         System.out.println("Su posicion es "+marioB.getPosicion()); 
+
+         System.out.println("d-->avanza, w-->salta, z-->salir"); 
+
+         while (bandera!=true){ 
+
+             tecla = lector.next().charAt(0); 
+
+            tecla = Character.toLowerCase(tecla); 
+
+             
+
+            
+
+            switch (tecla) { 
+
+                case 'd': 
+
+                    marioB.avanzarUnPaso(); 
+
+                    break; 
+
+                case 'w': 
+
+                    marioB.saltarObstaculo(); 
+
+                    break; 
+
+                case 'z': 
+
+                    System.out.println("saliendo del juego"); 
+
+                    bandera=true; 
+
+                    break; 
+
+                default: 
+
+                    System.out.println("Tecla inválida"); 
+
+            } 
+
+             
+
+             if(marioB.getPosicion()%10==0 && marioB.getPosicion()!=0 ){ 
+
+                System.out.println("Hay un obstaculo, saltaaaaaaaa"); 
+
+            } 
+
+             
+
+            if(marioB.getPosicion()%10==1 && (marioB.getSaltando()==false) && (marioB.getPosicion()!=1) ){ 
+
+                if(marioB.getVidas()>1){ 
+
+                    marioB.setVidas(marioB.getVidas()-1); 
+                    
+
+                }else{ 
+
+                    System.out.println(marioB.getNombre()+" muere..."); 
+
+                    bandera=true; 
+
+                } 
+
+            } 
+
+          System.out.println("su vida es: "+marioB.getVidas());
+
+         } 
+
+          
+
+          
+
+    } 
+
+} 
